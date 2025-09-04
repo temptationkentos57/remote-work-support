777e577f991545b73f371c17ca98334f95eb5efa
temptationkentos57 <kreetlannoo@hotmail.com>
2025-09-04 19:50:06 +0200

    Change alert message to include a thank you note.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    alert('Login successful! You will be redirected to your dashboard shortly.');
+    alert('Thank you for logging in! You will be redirected to your dashboard shortly.');
 });
